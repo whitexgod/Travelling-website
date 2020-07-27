b0eab0b8edd87fd81d342446a91ab6d66736a291
Tuhin Mukherjee <59216687+whitexgod@users.noreply.github.com>
2020-07-27 17:58:57 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,6 +1,26 @@
-$('.navbar-nav>li>a').on('click', function(){
-    $('.navbar-collapse').collapse('hide');
-});
+
+const navSlide = function() {
+
+  const burger = document.querySelector('.burger');
+  const nav = document.querySelector('.nav-links');
+  const navLinks = document.querySelectorAll('.nav-links li');
+  //toggle nav
+  burger.addEventListener('click', function() {
+    nav.classList.toggle('nav-active');
+
+    navLinks.forEach((link, index) => {
+      if (link.style.animation){
+        link.style.animation='';
+      }
+      else{
+        link.style.animation = `change 0.3s ease forwards ${index / 7 + 1}s`;
+      }
+    });
+    burger.classList.toggle('toggle');
+  });
+};
+
+navSlide();
 
 
 window.sr = ScrollReveal();
